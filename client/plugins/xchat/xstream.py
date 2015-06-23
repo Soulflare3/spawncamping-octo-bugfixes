4b19620b26b718d5d753bd371e7a2f3cd4b02033
Robert <robert@sf3soft.net>
2015-06-23 02:37:57 -0400

    Actually a proper plugin now (multiplatform too!)

--- client/plugins/xchat/xstream.py
+++ client/plugins/xchat/xstream.py
@@ -1,18 +1,28 @@
 __module_name__ = "xstream" 
-__module_version__ = "0.1" 
+__module_version__ = "0.2" 
 __module_description__ = "Livestreamer bridge for He/xChat 2 written in Python" 
 
 import xchat
-import os
 
-def main(word, word_eol, userdata):
-	if len(word) == 2:
-		os.system("start \"xstream\" cmd /c livestreamer twitch.tv/%s best" % word[1]) #Quick fix
+quality = "best"
+server = ""
+channel = ""
+
+def main(word, word_eol, userdata):	
+	channel = xchat.get_info("channel")[1:]
+	if xchat.get_info("server")=="tmi.twitch.tv":
+		server = "twitch.tv"
+	if len(word) == 1:
+		connect(channel,server)
+	elif len(word) == 2:
+		connect(word[1],server)
 	else:
-		print "Usage: /xstream <channel>"	
-	#if xchat.get_list
+		print "Usage: /xstream <channel>"
 	return xchat.EAT_ALL
 
+def connect(channel,server):
+	xchat.command("exec livestreamer {0}/{1} {2}".format(server,channel,quality))
+	
 def unload_xstream(userdata):
 	print __module_name__, "unloaded"
 	
